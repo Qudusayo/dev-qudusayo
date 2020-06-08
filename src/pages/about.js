a0457d4adbdf476ed408ec931a18b07851249b1b
Qudusayo <51055890+Qudusayo@users.noreply.github.com>
2020-06-08 19:04:48 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,12 +13,12 @@ const AboutPage = () => {
             <h2 className={styles.heading}>Who am i ?</h2>
             <p>
                 I'm <b>Qudusayo</b> , a fullstack developer.<br />
-                My stacks of development are SQL, ReactJS, Gatsby, NodeJS, GraphQL, HTML, HandleBars, CSS, SCSS, and Jquery, but my loved stacks for development are React and SCSS for frontend development and Node for backend development and SQL for database
+                My stacks of development are SQL, ReactJS, Gatsby, NodeJS, GraphQL, HTML, HandleBars, CSS, SCSS, JavaScript, and Jquery, but my loved stacks for development are React and SCSS for frontend development, Node for backend development and SQL for database
             </p>
 
             <h2 className={styles.heading}>My Loved Works</h2>
             <p>
-                I loved algorithms and data structure most because I loved mathematics. I take challenges in some sites, so I loved <a href="https://" target="_blank" rel="noreferrer" style={{cursor: "pointer"}}>Codility</a> over <a href="https://" target="_blank"  rel="noreferrer" style={{cursor: "pointer"}}>Hackerrank</a> because <a href="https://" target="_blank" rel="noreferrer" style={{cursor: "pointer"}}>Codility</a> challenges with time and gives solution result in percentage due to the time complexity and test passed by the code.
+                I loved algorithms and data structure due to my passion for mathematics. I do take challenges in some sites, so I prefer <a href="https://codility.app" target="_blank" rel="noreferrer" style={{cursor: "pointer"}}>Codility</a> over <a href="https://hackerrank.com" target="_blank"  rel="noreferrer" style={{cursor: "pointer"}}>Hackerrank</a> because <a href="https://codility.app" target="_blank" rel="noreferrer" style={{cursor: "pointer"}}>Codility</a> challenges with time and gives solution result in percentage due to the time complexity and test passed by the code.
             </p>
 
             <h2 className={styles.heading}>Target</h2>
@@ -28,7 +28,7 @@ const AboutPage = () => {
 
             <h2 className={styles.heading}>Word For Programmers</h2>
             <p>
-                Coding all night dosen't make you better than other, but focusing and practicing. Try to follow a scheme of practice and challenge
+                Coding all night doesn't make you better than other, but focusing and practicing does . Try to follow a scheme of practice and challenge
             </p>
         </Layout>
     )
